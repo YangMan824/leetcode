8e0a9c5d32220f50325715c9fbb034af5fd6a043
YangMan824 <sonder_ym@163.com>
2021-02-06 21:44:24 +0800

    Yang

---
+++ 1389按指定顺序创建目标数组.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[]target=new int[nums.length];
+    	for (int i = 0; i < index.length; i++) {
+			if(target[index[i]]!=0) {
+				for (int j = target.length-1; j > index[i] ; j--) {
+					target[j]=target[j-1];
+				}
+				target[index[i]]=nums[i];
+			}else {
+				target[index[i]]=nums[i];
+			}
+		}
+    	return target;
+    }
+}
